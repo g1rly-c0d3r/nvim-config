387ffc4896cf53b7ca2342098563fa38b2321652
g1rly-c0d3r <girly_c0d3r@aol.com>
2026-01-26 11:26:11 -0600

    added treesitter back

--- init.lua
+++ init.lua
@@ -21,6 +21,7 @@ vim.pack.add({
 	'https://github.com/neovim/nvim-lspconfig.git',
 	'https://github.com/hrsh7th/cmp-path.git',
     'https://github.com/fortran-lang/fortls.git',
+    'https://github.com/nvim-treesitter/nvim-tree-sitter.git',
 })
 
 -- colorscheme
